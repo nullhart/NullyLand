40f791cb14fe4239e248367a905a584fc4d1649c
Blake Mastrud <Blakejosephmastrud@gmail.com>
2018-09-12 10:47:15 -0600

    List Comp - WIP

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,14 +1,26 @@
 <template>
-    <v-expansion-panel>
-        <v-expansion-panel-content v-for="(item,i) in data" :key="i">
-            <div slot="header">{{item.title}}</div>
-            <v-card>
-                <v-card-text>
-                    <code>{{item.text}}</code>
-                </v-card-text>
-            </v-card>
-        </v-expansion-panel-content>
-    </v-expansion-panel>
+
+    <v-card class="elevation-3 post-image" style="margin-left:auto;margin-right:auto;">
+        <v-card-text class="cyan elevation-0 white--text mb-0">
+            <p class="font-weight-bold title mb-0 text-xs-center">{{data.title}}</p>
+        </v-card-text>
+        <div>
+            <v-list v-for="(item, i) in data.content" :key="i" subheader dense one-line>
+                <v-list-tile>
+                    <v-list-tile-content>
+                        <v-list-tile-title>
+                            <a :href="item.url" target="_blank">{{item.title}}</a>
+                        </v-list-tile-title>
+
+                    </v-list-tile-content>
+                </v-list-tile>
+
+                <v-divider v-if="i + 1 < data.content.length"></v-divider>
+            </v-list>
+
+        </div>
+    </v-card>
+
 </template>
 
 <script>
