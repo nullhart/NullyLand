76bd1cab3b69bfa13cafd7049f738ca2c83e618d
Blake Mastrud <Blakejosephmastrud@gmail.com>
2018-09-12 10:49:16 -0600

    Post view now renders a dummy article

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,76 +1,28 @@
 <template>
-    <v-container>
-        <v-flex xs12>
-            <h1 class="text-xs-center" editable>{{post.title}}</h1>
-            <vue-markdown :source="post.markdown"></vue-markdown>
-        </v-flex>
-    </v-container>
+  <v-container>
+
+    <h1>{{selectedPost.title}}</h1>
+    <v-divider></v-divider>
+
+    <div v-for="(things, i) in selectedPost.content" :key="i" style="perspective: 600px;">
+      <transition name="stagger">
+        <dynamic-sub-step style="margin: auto;" :data="things" class="AllowClick" :style="'animation-delay: ' + 300 * i + 'ms !important; will-change: auto; '" v-show="mounted" />
+      </transition>
+    </div>
+
+  </v-container>
 </template>
 
 <script>
-import VueMarkdown from "vue-markdown";
-
-import marked from "marked";
+import DynamicSubStep from "../components/DynamicSubStep";
 
 export default {
   name: "Post",
-  components: { VueMarkdown, marked },
+  components: { DynamicSubStep },
   data() {
     return {
       mounted: false,
-      post: {
-        id: 1,
-        dateCreated: new Date(),
-        title: "Raspberry Pi Router!",
-        markdown: `
----
-__Advertisement :)__
-
-- __[pica](https://nodeca.github.io/pica/demo/)__ - high quality and fast image
-  resize in browser.
-- __[babelfish](https://github.com/nodeca/babelfish/)__ - developer friendly
-  i18n with plurals support and easy syntax.
-
-You will like those projects!
-
----
-
-# h1 Heading 8-)
-## h2 Heading
-### h3 Heading
-#### h4 Heading
-##### h5 Heading
-###### h6 Heading
-
-
-## Horizontal Rules
-
-___
-
----
-
-***
-
-
-## Typographic replacements
-
-Enable typographer option to see result.
-
-(c) (C) (r) (R) (tm) (TM) (p) (P) +-
-
-test.. test... test..... test?..... test!....
-
-!!!!!! ???? ,,  -- ---
-
-"Smartypants, double quotes" and 'single quotes'
-
-
-
-
-      
-        `,
-        links: []
-      }
+      selectedPost: this.$store.state.applicationData.post
     };
   },
 
@@ -80,9 +32,37 @@ test.. test... test..... test?..... test!....
       return marked(this.post.content[index].text, { sanitize: false });
     }
   },
-  computed: {}
+  computed: {},
+  mounted() {
+    this.mounted = true;
+  }
 };
 </script>
 
-<style scoped>
+<style >
+@keyframes rotateWobble {
+  0% {
+    transform: rotateX(120deg);
+    opacity: 0;
+  }
+
+  50% {
+    transform: rotateX(-20deg);
+  }
+  100% {
+    transform: rotateX(0deg);
+    opacity: 1;
+  }
+}
+
+.stagger-leave {
+  animation-name: rotateWobble reverse;
+  animation-duration: 1s;
+}
+
+.stagger-enter-to {
+  transform: rotateX(90deg);
+  animation-name: rotateWobble;
+  animation-duration: 1s;
+}
 </style>
