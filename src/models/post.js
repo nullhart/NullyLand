026b94e7d7907d0329f848462c126c89386b2f81
Blake Mastrud <Blakejosephmastrud@gmail.com>
2018-09-12 10:48:21 -0600

    Post Structure Reference - Dev ref only - WIP

--- src/models/post.js
+++ src/models/post.js
@@ -1,39 +1,6 @@
-const Joi = require('joi');
-
-const postTemplate = Joi.object().keys({
-    id: Joi.number(),
-    dateCreated: Joi.date(),
-    title: Joi.string(),
-    content: Joi.object()
-})
-
-var newPost = async (post) => {
-
-
-    return await Joi.validate({
-        id: post.id,
-        dateCreated: new Date(),
-        title: post.title,
-        content: {
-            testObject: 'test'
-        }
-    }, postTemplate, (err, value) => {
-        if (err) {
-            return err.details
-        }
-        return value
-
-    });
-
+post = {
+    id: 1, //Unique Article Id
+    dateCreated: new Date(), //Original Author Date
+    title: "How to Setup MagicMirror²", //Article Title
+    content: [] //Array Off Dynamic Elements
 }
-
-var test = newPost({
-    id: 1,
-    title: 'hello'
-})
-
-console.log(test)
-
-// module.exports = {
-//     postTemplate
-// }
