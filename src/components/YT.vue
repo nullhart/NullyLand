7a9c7e2a8a9cc48f3e78526063209cfccddbd018
Blake Mastrud <Blakejosephmastrud@gmail.com>
2018-09-12 10:47:51 -0600

    Add YouTube Embed Comp - WIP

---
+++ src/components/YT.vue
@@ -0,0 +1,26 @@
+<template>
+    <div style="margin-left:auto;margin-right:auto;">
+        <v-card-text class="red elevation-3 white--text ma-0">
+            <p class=" mb-0 text-xs-center title font-weight-bold">
+                {{data.title}}
+            </p>
+        </v-card-text>
+        <iframe class="elevation-3" width="100%" height="360" :src="data.url" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    </div>
+</template>
+<style scoped>
+code {
+  box-shadow: none;
+}
+</style>
+
+
+<script>
+export default {
+  name: "yt",
+  components: {},
+  props: ["data"]
+};
+</script>
+
+
