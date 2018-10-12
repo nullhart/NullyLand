8bc6e75229de1d579e054fa8bcaff9a03aa7b25e
Blake Mastrud <Blakejosephmastrud@gmail.com>
2018-10-12 09:18:39 -0600

    Small tweaks - Cleaning up

--- src/models/post.js
+++
@@ -1,6 +0,0 @@
-post = {
-    id: 1, //Unique Article Id
-    dateCreated: new Date(), //Original Author Date
-    title: "How to Setup MagicMirror²", //Article Title
-    content: [] //Array Off Dynamic Elements
-}
