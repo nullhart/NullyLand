19b71a26d98e4d307c5caa8a4b86f204ee9eccb2
Blake Mastrud <Blakejosephmastrud@gmail.com>
2018-10-16 18:30:58 -0600

    *Better image placeholder gradient
    * update date formatting
    
    * More article fade in transition

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -1,12 +1,12 @@
-<template>
+<template >
+  <transition-group name="fade" tag="v-layout" v-show="mounted" class="manual-v-layout noClick" style="max-width: 1500px;margin: auto; ">
 
-  <v-container class="noClick" v-show="mounted" fluid grid-list-md>
-    <v-layout wrap>
-      <v-flex v-for="(post, index) in posts" xs12 sm6 md4 xl4 :key="index" v-bind:data-index="index" @click="$router.push('/Post')">
-        <v-card class="minny AllowClick" style="margin-left:auto;margin-right:auto;">
+    <v-flex transition="fade" class="pa-4" v-for="(post, index) in this.$store.state.mainPosts" xs12 sm6 md4 xl4 :key="index" v-bind:data-index="index" @click="$router.push('/Post')">
+      <v-hover close-delay="0">
+        <v-card slot-scope="{ hover }" :class="`elevation-${hover ? 12 : 2}`" class="minny AllowClick" style="margin-left:auto;margin-right:auto; ">
           <v-responsive>
 
-            <v-img :aspect-ratio="16/9" class="orange" transition="imageFade" :src="'https://picsum.photos/600/400?random' + Math.random()">
+            <v-img :aspect-ratio="16/9" class="generalGradient" transition="imageFade" :src="post.thumbnail">
               <v-layout slot="placeholder" fill-height align-center justify-center ma-0>
                 <v-progress-circular class="" indeterminate color="yellow"></v-progress-circular>
               </v-layout>
@@ -16,7 +16,7 @@
           <v-card-title primary-title>
             <div>
               <div class="headline">{{post.title}}</div>
-              <span class="grey--text">{{trimed(post.description, 120)}}</span>
+              <span class="grey--text">{{post.description}}</span>
             </div>
           </v-card-title>
 
@@ -29,55 +29,32 @@
               <v-icon>share</v-icon>
             </v-btn>
             <v-spacer></v-spacer>
-            <v-chip class="secondary white--text">{{formatedDate(post.postedDate)}}</v-chip>
+            <v-chip class="secondary white--text">{{formatedDate(post.dateCreated.seconds)}}</v-chip>
           </v-card-actions>
         </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+      </v-hover>
+    </v-flex>
+
+  </transition-group>
 
 </template>
+
+
 <script>
+import firebase from "firebase/app";
+import "firebase/auth";
+import db from "../helpers/firebaseInit";
+
 export default {
   name: "Posts",
   components: {},
   data() {
     return {
-      posts: [
-        {
-          title: "Raspberry Pi Spy Cam",
-          description: "this is a test description that is way to long",
-          postedDate: new Date()
-        },
-        {
-          title: "Raspberry Pi Spy Cam",
-          description: "this is a test description that is way to long",
-          postedDate: new Date()
-        },
-        {
-          title: "Raspberry Pi Spy Cam",
-          description: "this is a test description that is way to long",
-          postedDate: new Date()
-        },
-        {
-          title: "Raspberry Pi Spy Cam",
-          description: "this is a test description that is way to long",
-          postedDate: new Date()
-        },
-        {
-          title: "Raspberry Pi Spy Cam",
-          description: "this is a test description that is way to long",
-          postedDate: new Date()
-        },
-        {
-          title: "Raspberry Pi Spy Cam",
-          description: "this is a test description that is way to long",
-          postedDate: new Date()
-        }
-      ],
+      posts: [],
       mounted: false
     };
   },
+
   mounted() {
     this.$nextTick().then(data => {
       this.mounted = true;
@@ -88,25 +65,52 @@ export default {
       return string.substring(0, max) + "...";
     },
     formatedDate: function(date) {
-      return new Date(date).toDateString();
+      return new Date(new Date(1970, 0, 1).setSeconds(date)).toDateString();
     }
   }
 };
 </script>
 
-<style >
+<style  >
+.fade-enter-active,
+.fade-move,
+.fade-leave-active {
+  transition: opacity 2s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+
 .imageFade-enter-active {
-  transition: opacity 1s ease-in-out;
+  transition: opacity 2s ease-in-out;
 }
 .imageFade-enter-to {
-  opacity: 1;
+  transition: opacity 2s ease-in-out;
 }
 .imageFade-enter {
   opacity: 0;
 }
 .imageFade-move {
-  transition: 1s ease-in-out;
+  transition: 2s ease-in-out;
 }
+
+.manual-v-layout {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-flex: 1;
+  -ms-flex: 1 1 auto;
+  flex: 1 1 auto;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  padding-bottom: 8px !important;
+  padding-top: 8px !important;
+}
+
 .minny {
   max-width: 400px;
   min-width: 320px;
