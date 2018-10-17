671323bb4a57c13d316135c1c691e111ab8edd9e
Blake Mastrud <Blakejosephmastrud@gmail.com>
2018-10-16 18:27:43 -0600

    WIP - Modifying login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,8 @@
 
 <template >
-  <div class="containerr">
-    <transition name="grow" mode="out-in">
-      <v-form key="0" class='login elevation-24' v-if="$store.state.applicationState.login.modal == 'Login'">
+  <div>
+    <transition name="fade" mode="out-in">
+      <v-form key="0" class='login elevation-24 white' v-if="$store.state.applicationState.login.modal == 'Login'">
         <v-flex class="error" style="width: 5px;">
           <h2 class="error--text" style="position: relative; left: 20px;">Login</h2>
         </v-flex>
@@ -14,16 +14,14 @@
             <v-text-field @keyup.enter="login" validate-on-blur v-model="password" :append-icon="showPass ? 'visibility_off' : 'visibility'" :rules="[rules.required, rules.min]" :type="showPass ? 'text' : 'password'" label="Password" hint="At least 8 characters" counter @click:append="showPass = !showPass"></v-text-field>
           </v-flex>
           <v-flex class="loginButton">
-            <v-btn round class="error elevation-6" large :loading="LoginProgress" @click="login">Login</v-btn>
+            <v-btn round class=" elevation-6 white--text font-weight-bold" large :loading="LoginProgress" @click="login">Login</v-btn>
           </v-flex>
         </v-container>
-
       </v-form>
-      <v-form key="1" class='login elevation-24' v-if="$store.state.applicationState.login.modal == 'Sign Up'">
+      <v-form key="1" class='login elevation-24 white' v-if="$store.state.applicationState.login.modal == 'Sign Up'">
         <v-flex class="error" style="width: 5px;">
           <h2 class="error--text" style="position: relative; left: 20px; width: 300px;">Sign Up</h2>
         </v-flex>
-
         <v-container style="height: 100%;">
           <v-flex xs12>
             <v-text-field validate-on-blur v-model="registerEmail" :rules="[rules.required, rules.email]" label="E-mail"></v-text-field>
@@ -32,12 +30,10 @@
             <v-text-field @keyup.enter="register" validate-on-blur v-model="registerPassword" :append-icon="showPass ? 'visibility_off' : 'visibility'" :rules="[rules.required, rules.min]" :type="showPass ? 'text' : 'password'" label="Password" hint="At least 8 characters" counter @click:append="showPass = !showPass"></v-text-field>
           </v-flex>
           <v-flex class="loginButton">
-            <v-btn round class="error elevation-6" large :loading="RegisterProgress" @click="register">Sign Up</v-btn>
+            <v-btn round class="elevation-6 white--text font-weight-bold" large :loading="RegisterProgress" @click="register">Sign Up</v-btn>
           </v-flex>
         </v-container>
-
       </v-form>
-
     </transition>
 
   </div>
@@ -125,65 +121,15 @@ export default {
 };
 </script>
 <style scoped>
-
-.slideRight-leave-active,
-.slideRight-enter-active {
-  transition: 1s;
-}
-.slideRight-enter {
-  -webkit-transform: translateX(300%);
-  transform: translateX(300%);
-  transition: transform 300ms linear;
-  will-change: transform;
-}
-.slideRight-leave-to {
-  transform: translate(300%, 0);
-}
-
-.slideLeft-leave-active,
-.slideLeft-enter-active {
-  transition: 1s;
-}
-.slideLeft-enter {
-  transform: translate(-300%, 0);
-}
-.slideLeft-leave-to {
-  transform: translate(-300%, 0);
-}
-
-.err {
-  background-color: #e1645f;
-  animation: wiggle 0.3s;
-}
-
-.grow-enter-active {
-  animation: grow 0.8s;
-}
-.grow-leave-active {
-  animation: grow_left 0.8s reverse;
-}
-
-.containerr {
-  padding: 0px;
-  overflow: hidden;
-  z-index: 1;
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: 0.8fr 2fr 0.8fr;
-  grid-template-areas:
-    "head head head head"
-    "main main main main"
-    "footer footer footer footer";
-}
-
 .login {
-  margin: auto;
-  grid-area: main;
+  margin: 0 auto;
+  width: 100px;
+  transform: translateY(50%);
+  border-radius: 20px;
   padding-top: 15px;
   min-width: 290px;
   max-width: 390px;
   min-height: 300px;
-  overflow: hidden;
 }
 
 .loginButton {
@@ -191,6 +137,10 @@ export default {
   align-items: center;
   justify-content: center;
   transform: translateY(60%);
+}
+
+.loginButton button {
+  background: linear-gradient(#f3d250, #fe5f55);
 }
 
 @keyframes grow {
