2e28fc4e7615daade869b189e3a7f2aa3a9287e2
Blake Mastrud <Blakejosephmastrud@gmail.com>
2018-11-22 12:23:52 -0700

    Add New Post Element - Plain Text Box

---
+++ src/components/textChunk.vue
@@ -0,0 +1,22 @@
+
+
+<template>
+    <v-card v-show="loaded" dark class=" subheading font-weight-bold  elevation-3" style="margin-left:auto;margin-right:auto; background-color: rgb(39, 40, 34);">
+        <v-card-text style="text-indent: 2em">{{data.text}}</v-card-text>
+    </v-card>
+</template>
+
+<script>
+export default {
+  name: "textChunk",
+  props: ["data"],
+  data: function() {
+    return {
+      loaded: false
+    };
+  },
+  mounted() {
+    this.loaded = true;
+  }
+};
+</script>
