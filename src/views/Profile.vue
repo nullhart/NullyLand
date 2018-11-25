2550c22bee7d940aeddc2f012fa79b8ac6e1d5b3
Blake Mastrud <Blakejosephmastrud@gmail.com>
2018-11-25 11:22:16 -0700

    Change Transition

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,31 +1,34 @@
 
 <template >
   <v-container>
-    <v-flex class="elevation-3 white mt-5 xs10 sm6 md4  pa-2" style="margin: auto;" v-on:keypress.enter="update($store.state.UserProfile.id, $store.state.UserProfile)">
-      <v-form v-show="$store.state.applicationState.loaded">
-        <v-flex class="text-xs-center">
-          <v-avatar size="150" class="ma-4" color="blue lighten-4">
-            <img v-show="this.$store.state.UserProfile.profileImage != null || file_changed ==true" :size=23 :src="this.$store.state.UserProfile.profileImage" @click="onChange($event)">
-            <input @input="onChange($event)" placeholder="tst" type="file" name="file" accept="image/*" style="border-radius: 100%;opacity: 0; z-index: 2; width: 150px; height: 150px; position: absolute; left: 0px; top: 0px; cursor: pointer;">
-            <div v-show="this.$store.state.UserProfile.profileImage == null">
-              <v-icon>mdi-account</v-icon>
-            </div>
-          </v-avatar>
-        </v-flex>
+    <transition name="slide-x-transition">
+      <v-flex v-show="$store.state.applicationState.loaded && mounted" class="elevation-3 white mt-5 xs10 sm6 md4  pa-2" style="margin: auto;" v-on:keypress.enter="update($store.state.UserProfile.id, $store.state.UserProfile)">
+        <v-form>
+          <v-flex class="text-xs-center">
+            <v-avatar size="150" class="ma-4" color="blue lighten-4">
+              <img v-show="this.$store.state.UserProfile.profileImage != null || file_changed ==true" :size=23 :src="this.$store.state.UserProfile.profileImage" @click="onChange($event)">
+              <input @input="onChange($event)" placeholder="tst" type="file" name="file" accept="image/*" style="border-radius: 100%;opacity: 0; z-index: 2; width: 150px; height: 150px; position: absolute; left: 0px; top: 0px; cursor: pointer;">
+              <div v-show="this.$store.state.UserProfile.profileImage == null">
+                <v-icon>mdi-account</v-icon>
+              </div>
+            </v-avatar>
+          </v-flex>
 
-        <v-divider></v-divider>
-        <v-text-field v-model="$store.state.UserProfile.name" :counter="20" label="Name"></v-text-field>
-        <v-text-field v-show="$store.state.applicationState.loaded" v-model="formattedPhone" label="Phone Number"></v-text-field>
+          <v-divider></v-divider>
+          <v-text-field v-model="$store.state.UserProfile.name" :counter="20" label="Name"></v-text-field>
+          <v-text-field v-show="$store.state.applicationState.loaded" v-model="formattedPhone" label="Phone Number"></v-text-field>
 
-        <v-flex class="text-xs-center">
-          <v-btn round :loading="$store.state.applicationState.loading == true" :disabled="!$store.state.applicationState.online" large @click="update($store.state.UserProfile.id, $store.state.UserProfile)" color="orange" :dark="$store.state.applicationState.online">
-            <v-icon>save</v-icon>
-          </v-btn>
-        </v-flex>
+          <v-flex class="text-xs-center">
+            <v-btn round :loading="$store.state.applicationState.loading == true" :disabled="!$store.state.applicationState.online" large @click="update($store.state.UserProfile.id, $store.state.UserProfile)" color="orange" :dark="$store.state.applicationState.online">
+              <v-icon>save</v-icon>
+            </v-btn>
+          </v-flex>
 
-      </v-form>
+        </v-form>
 
-    </v-flex>
+      </v-flex>
+    </transition>
+
   </v-container>
 </template>
   
