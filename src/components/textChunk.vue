a2bcf3d169ec3e3cd858252fae68eb8c453e99b1
Blake Mastrud <Blakejosephmastrud@gmail.com>
2018-11-25 10:16:37 -0700

    Minor Style Change

--- src/components/textChunk.vue
+++ src/components/textChunk.vue
@@ -1,9 +1,9 @@
 
 
 <template>
-    <v-card v-show="loaded" dark class=" subheading font-weight-bold  elevation-3" style="margin-left:auto;margin-right:auto; background-color: rgb(39, 40, 34);">
-        <v-card-text style="text-indent: 2em">{{data.text}}</v-card-text>
-    </v-card>
+  <v-card v-show="loaded" class=" subheading font-weight-bold  elevation-3 white--text" style="margin-left:auto;margin-right:auto; background-color: #272822">
+    <v-card-text style="text-indent: 2em">{{data.text}}</v-card-text>
+  </v-card>
 </template>
 
 <script>
